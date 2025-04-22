06ed948077d57d4f9a87050a9d08eae573cec1dd
Lisa <lisatan0812@gmail.com>
2025-04-22 02:11:26 +0000

    initial commit for FE

---
+++ lisa/mattergen-app/frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // this makes Vite accessible from outside the container
+    port: 5173,      // optional, default is 5173
+  },
+})
+
