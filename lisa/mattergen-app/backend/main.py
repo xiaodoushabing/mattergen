bb1fe1e63bc52e311671dfc2d7febe551229ff38
Lisa <lisatan0812@gmail.com>
2025-04-23 09:27:09 +0000

    Added CORS Middleware

--- lisa/mattergen-app/backend/main.py
+++ lisa/mattergen-app/backend/main.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI
 from routes import generate_lattice, retrieval, download
 from database import connect_to_mongo
 from core.middleware_config import LogRequestMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="MatterGen & MatterSim API")
+app = FastAPI(title="MatBuddy API")
 
+# define allowed origins
+origins = [
+    "http://localhost:5173", # Vite default dev server
+]
+
+# add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, # Allows requests that include credentials like cookies
+    allow_methods=["*"],  # Allows all standard HTTP methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],  # Allows all request headers
+)
+
+# add custom logging middleware
 app.add_middleware(LogRequestMiddleware)
 
 @app.on_event("startup")
